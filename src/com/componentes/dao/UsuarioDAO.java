f54058adbc72949154d02595b7a64eb3bd5e7721
Andres <43399076+cotoa412@users.noreply.github.com>
2019-11-16 21:44:25 -0600

    Anthony no sabe ver (UsuarioDAO y Usuario)

--- src/com/componentes/dao/UsuarioDAO.java
+++ src/com/componentes/dao/UsuarioDAO.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class UsuarioDAO<Usuario> extends Servicio implements IDao<Usuario> {
 
+	
 	@Override
-	public void Insert(Usuario t) {
-		try {
-		System.out.println("Entro......");
+	public void Insert(Usuario t) { 
+		
 		this.startEntityManagerFactory();
 		this.em.getTransaction().begin();
 		
@@ -15,34 +15,57 @@ public class UsuarioDAO<Usuario> extends Servicio implements IDao<Usuario> {
 		
 		this.em.getTransaction().commit();
 		this.stopEntityManagerFactory();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		
 	}
 
 	@Override
 	public void Update(Usuario t) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void Delete(Usuario t) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public Usuario Get(int id) {
-
 		
 		return null;
+	}
+	
+	public Usuario getUsuario(int id,Usuario u) {
+		
+		
+		
+		this.startEntityManagerFactory();
+		Usuario userLeido = (Usuario) em.find(u.getClass(), new Integer(id));
+		this.stopEntityManagerFactory();
+		return (Usuario) userLeido; 
+		
 	}
 
 	@Override
 	public List<Usuario> GetList() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
+	public List<Usuario> getListUsuario(Usuario u){
+	
+		
+		this.startEntityManagerFactory();
+		
+		this.em.getTransaction().begin();
+		
+
+		TypedQuery<Usuario> query = (TypedQuery<Usuario>)em.createNamedQuery("Usuario.findPK", u.getClass());
+		this.stopEntityManagerFactory();
+
+		
+		return query.getResultList();
+
+	}
+	
+	
 }
--- src/com/componentes/tester/UsuarioTester.java
+++ src/com/componentes/tester/UsuarioTester.java
@@ -5,17 +5,91 @@ import com.componentes.entidades.Usuario;
 
 public class UsuarioTester {
 
+
+	private static EntityManagerFactory entityManagerFactory = null;
+	private static EntityManager em = null;
+
+
 	public static void main(String[] args) {
+			try {
+			
+			startEntityManagerFactory();
+		/*	
+			Usuario u = new Usuario();
+			u.setNombre("Anthony");
+			u.setConstrania("qwop123!");
+			u.setCorreo("cotoandres2018@gmail.com");
+			
+			
+			em.getTransaction().begin();
+			
+			em.persist(u);
+			
+			em.getTransaction().commit();
 		
-		UsuarioDAO dao = new UsuarioDAO();
-		
-		Usuario usuario = new Usuario();
-		usuario.setNombre("Andre");
-		usuario.setCorreo("Anthonyestiven@gmail.com");
-		usuario.setConstrania("123456");
-		
-		dao.Insert(usuario);
+			*/
+			
+			Usuario u = new Usuario();
+			
+			System.out.println(" " + findPK().getNombre());
+
+			System.out.println(" " + findPK().getConstrania());
+			
+			UsuarioDAO<Usuario> ud = new UsuarioDAO<Usuario>();
+			
+			System.out.println(ud.getUsuario(1, u).getNombre());
+			
+
+			System.out.println(ud.getUsuario(1, u).getConstrania());
+			
+			
+
+			
+			stopEntityManagerFactory();
 		
 	}
 
+	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static Usuario findPK() throws Exception {
+		Usuario usuario = (Usuario)em.createNamedQuery("Usuario.findPK").
+											setParameter("idParam", new Integer("1")).
+											getSingleResult();
+		return usuario;
+	}
+	
+	
+	public static void startEntityManagerFactory() {
+		if (entityManagerFactory == null) {
+			try {
+				entityManagerFactory = Persistence
+						.createEntityManagerFactory("Componentes");
+				em = entityManagerFactory.createEntityManager();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void stopEntityManagerFactory() {
+		if (entityManagerFactory != null) {
+			if (entityManagerFactory.isOpen()) {
+				try {
+					entityManagerFactory.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			em.close();
+			entityManagerFactory = null;
+		}
+	}
+   
+   
 }
+
